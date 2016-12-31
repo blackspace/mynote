c9592ffb3180485707bf998712eee4c7a70b91ec
blackspace <weiwufeng@sohu.com>
2016-12-31 16:00:25 +0800

    .

--- oracle/script/create_database.sh
+++ oracle/script/create_database.sh
@@ -1,59 +1,61 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
-echo Create Oracle DATABASE
-echo ======================
 
 MY_ORACLE=~/myoracle
 
-DATA=$MY_ORACLE/data
-CTL_LOG1=$MY_ORACLE/ctl_log_1
-CTL_LOG2=$MY_ORACLE/ctl_log_2
-ARCHIVE_LOG=$MY_ORACLE/archive_log
-ARCHIVE_LOG_DUPLEX=$MY_ORACLE/archive_log_duplex
+OD_DATA=$MY_ORACLE/data
+OD_CTL_LOG1=$MY_ORACLE/ctl_log_1
+OD_CTL_LOG2=$MY_ORACLE/ctl_log_2
+OD_ARCHIVE_LOG=$MY_ORACLE/archive_log
+OD_ARCHIVE_LOG_DUPLEX=$MY_ORACLE/archive_log_duplex
 
-
-
-for d in $DATA $CTL_LOG1 $CTL_LOG2 $ARCHIVE_LOG $ARCHIVE_LOG_DUPLEX;do
-    mkdir -p $d
-    echo create $d
+for od in ${!OD*};do
+   if [ -d ${!od}  ]; then
+       echo $od is exist. 
+   else
+       mkdir  -vp ${!od}
+   fi
 done
 
-echo --------------------------------
+OLD_DBS_FILES=$(find $ORACLE_HOME/dbs -iname "*$ORACLE_SID*")
 
-
-INIT=${ORACLE_HOME:-~}/dbs/init$ORACLE_SID.ora
-
-if [ -f $INIT ];then
-    rm $INIT
-    echo rm $INIT
-    echo -----------------------------
+if [ -n "$OLD_DBS_FILES" ]; then
+    for o in $OLD_DBS_FILES; do
+	rm -v $o
+    done
 fi
 
-tee $INIT <<EOF 
+PFILE=$ORACLE_HOME/dbs/init$ORACLE_SID.ora
+
+tee $PFILE <<EOF 
 db_name=orcl
 sga_target=200M
 sga_max_size=1G
-db_create_file_dest=$DATA
-db_create_online_log_dest_1=$CTL_LOG1
-db_create_online_log_dest_2=$CTL_LOG2
-log_archive_dest=$ARCHIVE_LOG
-log_archive_duplex_dest=$ARCHIVE_LOG_DUPLEX
+db_create_file_dest=$OD_DATA
+db_create_online_log_dest_1=$OD_CTL_LOG1
+db_create_online_log_dest_2=$OD_CTL_LOG2
+log_archive_dest=$OD_ARCHIVE_LOG
+log_archive_duplex_dest=$OD_ARCHIVE_LOG_DUPLEX
 EOF
 
-echo 
-echo Make a $INIT 
-echo --------------------------
 
 cat <<'EOF'
-Now you can create database,open the sqlplus and execute the below command:
+Now you can create database,open the sqlplus as sysdba and execute the below command:
 1. startup nomount
-2. create database
+2. create database;
 3. create spfile from memory;
 4. create temporary tablespace temp;
 5. alter database default temporary tablespace temp;
 6. alter system set sga_target='1g';
 7. @?/rdbms/admin/catalog
 8. @?/rdbms/admin/catproc.sql
-9. alter system switch logfile; 
+9. alter user system identified by oracle
+10.connect system/oracle
+11.@?/sqlplus/admin/pupbld
+12.connect / as sysdba
+13.alter system switch logfile; 
 EOF
+
+
+
 
